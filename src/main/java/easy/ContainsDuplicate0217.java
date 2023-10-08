d4b77f7406d7a3cf33342d261be0c4d75d3bb137
NiceLeader <maciek2002.10@wp.pl>
2023-10-08 23:02:19 +0200

    Problem 217 containsDuplicate, done

--- src/main/java/easy/ContainsDuplicate0217.java
+++ src/main/java/easy/ContainsDuplicate0217.java
@@ -1,4 +1,18 @@
 package easy;
 
-public class ContainsDuplicate0217 {
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for  (int num : nums){
+            if (set.contains(num))
+            {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
 }
